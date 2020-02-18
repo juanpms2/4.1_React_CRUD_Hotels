7be640c36304c1f589b0596b4c19b7da7cd20938
juanpms2 <jpablo69@hotmail.es>
2020-02-18 20:16:18 +0100

    Instalación y configuración inicial del proyecto

---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
+import { LoginScene, HotelCollectionScene } from "./scenes";
+import { switchRoutes } from "core";
+
+ReactDOM.render(
+	<HashRouter>
+		<Switch>
+			<Route
+				exact={true}
+				path={[switchRoutes.root, switchRoutes.login]}
+				component={LoginScene}
+			/>
+			<Route
+				path={switchRoutes.hotelCollection}
+				component={HotelCollectionScene}
+			/>
+		</Switch>
+	</HashRouter>,
+	document.getElementById("root")
+);
